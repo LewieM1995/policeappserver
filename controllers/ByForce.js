fb2b96847727a540c5350c582ac1cea5f3df8fde
LewieM1995 <lewiemarks@gmail.com>
2023-12-10 16:27:01 +0000

    Updates ByForce.js

--- controllers/ByForce.js
+++ controllers/ByForce.js
@@ -50,7 +50,7 @@ exports.ByForce = async (req, res) => {
   const existingData = await connection.execute(query, values);
 
   if (existingData[0].length > 0) {
-    // Data found. Retrieving from cache
+    console.log('Data found. Retrieving from cache');
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(existingData[0]);
     return;
@@ -58,13 +58,6 @@ exports.ByForce = async (req, res) => {
 
 
   const insertQuery = 'INSERT INTO stops (id, age_range, self_defined_ethnicity, outcome_linked_to_object_of_search, datetime, removal_of_more_than_outer_clothing, operation, officer_defined_ethnicity, object_of_search, involved_person, gender, legislation, location_latitude, location_street_id, location_street_name, location_longitude, outcome, type, operation_name, forcename, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
-  if (dataWithIds.length === 0) {
-    // No new data from API. Returning 204 No Content
-    console.log('No new data from API. Returning 204 No Content.');
-    res.status(204).send();
-    return;
-  }
 
   for (const item of dataWithIds) {
     const insertValues = [
