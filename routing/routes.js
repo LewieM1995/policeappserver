c6b2773f8f2c789d64d7ca7f23f5bd85be587681
LewieM1995 <Lewiemarks@gmail.com>
2024-09-03 18:59:10 +0100

    architecture

--- routing/routes.js
+++ routing/routes.js
@@ -1,25 +1,20 @@
 const express = require('express');
 
-// import controllers for policeapp
+// Import controllers
 const { ByLocation } = require('../controllers/ByLocation');
 const { ByForce } = require('../controllers/ByForce');
 const { GetDataById } = require('../controllers/DataById');
-
-// import controllers for ringcon
 const { postData } = require('../controllers/postData');
 const { getData } = require('../controllers/getData');
 const { getDataTable } = require('../controllers/getDataTable');
-
-// import controllers for Fuji Seal
 const { addOrUpdatePantone } = require('../controllers/fujisealControllers/addOrUpdatePantone');
 const { getUser } = require('../controllers/fujisealControllers/userControllers/getUser');
-const {getChart} = require('../controllers/fujisealControllers/getChart')
-const {postDataFuji} = require('../controllers/fujisealControllers/postData');
-const {getPantones} = require('../controllers/fujisealControllers/getPantones');
-const {postQuarantineInk} = require('../controllers/fujisealControllers/postQuarantineInk');
-const {updatePantone} = require('../controllers/fujisealControllers/updatePantone');
-const {deletePantone} = require('../controllers/fujisealControllers/deletePantone')
-
+const { getChart } = require('../controllers/fujisealControllers/getChart');
+const { postDataFuji } = require('../controllers/fujisealControllers/postData');
+const { getPantones } = require('../controllers/fujisealControllers/getPantones');
+const { postQuarantineInk } = require('../controllers/fujisealControllers/postQuarantineInk');
+const { updatePantone } = require('../controllers/fujisealControllers/updatePantone');
+const { deletePantone } = require('../controllers/fujisealControllers/deletePantone');
 
 // Routes for policeapp
 const policeAppRouter = express.Router();
@@ -27,7 +22,6 @@ policeAppRouter.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     next();
 });
-
 policeAppRouter.post('/location', ByLocation);
 policeAppRouter.post('/byforce', ByForce);
 policeAppRouter.get('/get-data/:id', GetDataById);
@@ -38,7 +32,6 @@ ringconRouter.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     next();
 });
-
 ringconRouter.post('/data', postData);
 ringconRouter.get('/data', getData);
 ringconRouter.get('/data_table', getDataTable);
@@ -49,7 +42,6 @@ fujiRouter.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
     next();
 });
-
 fujiRouter.post('/getUser', getUser);
 fujiRouter.post('/getChart', getChart);
 fujiRouter.post('/postData', postDataFuji);
@@ -57,7 +49,7 @@ fujiRouter.get('/getPantones', getPantones);
 fujiRouter.post('/addPantone', addOrUpdatePantone);
 fujiRouter.post('/postQuarantineInk', postQuarantineInk);
 fujiRouter.post('/updatePantone', updatePantone);
-fujiRouter.delete('/deletePantone/:id', deletePantone)
+fujiRouter.delete('/deletePantone/:id', deletePantone);
 
 module.exports = {
     policeAppRouter,
